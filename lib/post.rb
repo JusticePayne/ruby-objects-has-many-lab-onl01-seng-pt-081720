c5a71d356fe42118c967d61e9c310a40f72a47af
Justice Payne <spikepayne@yahoo.com>
2020-09-26 21:29:51 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,18 @@
 class Post
- attr_accessor :title, :author
- 
+  attr_accessor :author, :title
+  @@all = []
+
   def initialize(title)
     @title = title
-    @all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
   def author_name
     self.author != nil ? self.author.name : nil
   end
+
 end
