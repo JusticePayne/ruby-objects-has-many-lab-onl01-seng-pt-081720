5f396dff3e21e056b04d348c40927bff0242a0ed
Justice Payne <spikepayne@yahoo.com>
2020-09-26 21:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,24 @@
-  
 class Song
 
-    attr_accessor :name, :artist
+  attr_accessor :name, :artist
 
-    @@all =[]
 
-    def initialize(name)
-        @name = name
-        @@all << self
-    end
+  @@all = []
 
-    def artist_name
-      if artist_name
-        artist.name
-      else
-        nil
-      end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if artist
+      artist.name
+    else
+      nil
     end
   end
+end
